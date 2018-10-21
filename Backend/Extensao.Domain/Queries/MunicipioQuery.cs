29fdd4f3045081ccafe3c6d4ccfe2650e21f3204
Jéssica Gouveia <jessicacostagouveia23@gmail.com>
2018-10-21 02:39:50 -0300

    Restore

---
+++ Backend/Extensao.Domain/Queries/MunicipioQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extensao.Domain.Queries
+{
+    class MunicipioQuery
+    {
+        public int MyProperty { get; set; }
+    }
+}
